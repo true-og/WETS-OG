de56dcc2bff2952cc57074e821492227c391987d
NotAlexNoyle <alexnoyle@icloud.com>
2025-06-28 19:08:41 -0400

    Update gradle to 8.14.2

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,13 +1,7 @@
-import java.io.BufferedOutputStream
-import java.io.FileOutputStream
-import java.nio.file.Files
-import java.util.zip.ZipEntry
-import java.util.zip.ZipOutputStream
-
 plugins {
     id("java") // Tell gradle this is a java project.
     id("java-library") // Import helper for source-based libraries.
-    id("com.diffplug.spotless") version "7.0.4"
+    id("com.diffplug.spotless") version "7.0.4" // Import auto-formatter.
     id("com.gradleup.shadow") version "8.3.6" // Import shadow API.
     eclipse // Import eclipse plugin for IDE integration.
 }
@@ -18,20 +12,17 @@ java {
 }
 
 group = "net.trueog.wets-og" // Declare bundle identifier.
+
 version = "1.2" // Declare plugin version (will be in .jar).
+
 val apiVersion = "1.19"
 
 tasks.named<ProcessResources>("processResources") {
-    val props = mapOf(
-        "version" to version,
-        "apiVersion" to apiVersion
-    )
+    val props = mapOf("version" to version, "apiVersion" to apiVersion)
 
     inputs.properties(props) // Indicates to rerun if version changes.
 
-    filesMatching("plugin.yml") {
-        expand(props)
-    }
+    filesMatching("plugin.yml") { expand(props) }
     from("LICENSE") { // Bundle license into .jars.
         into("/")
     }
@@ -40,9 +31,7 @@ tasks.named<ProcessResources>("processResources") {
 repositories {
     mavenCentral()
     gradlePluginPortal()
-    maven {
-        url = uri("https://repo.purpurmc.org/snapshots")
-    }
+    maven { url = uri("https://repo.purpurmc.org/snapshots") }
     maven {
         url = uri("https://maven.enginehub.org/repo") // Get WorldEdit API from EngineHub Repository.
     }
@@ -68,9 +57,7 @@ tasks.build {
     dependsOn(tasks.shadowJar)
 }
 
-tasks.jar {
-    archiveClassifier.set("part")
-}
+tasks.jar { archiveClassifier.set("part") }
 
 tasks.withType<JavaCompile>().configureEach {
     options.compilerArgs.add("-parameters")
@@ -90,5 +77,9 @@ spotless {
     java {
         removeUnusedImports()
         palantirJavaFormat()
+    }
+    kotlinGradle {
+        ktfmt().kotlinlangStyle().configure { it.setMaxWidth(120) }
+        target("build.gradle.kts", "settings.gradle.kts")
     }
 }
